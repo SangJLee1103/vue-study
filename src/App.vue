f61d47e5beb8a0f4f7b2d4cd8b385e2a3fb4fc33
sangjun <jesusfollow1103@naver.com>
2021-12-20 20:13:49 +0900

    day1 DataBinding, v-for

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<!-- nav -->
+  <div class="menu">
+    <!-- :key=""의 용도 
+          반복문 사용시 꼭 써야함 반복문 돌린 요소를 컴퓨터가 구분하기 위해 사용-->
+    <a v-for="(menuList, i) in menu" :key="i"> {{ menuList }} </a>  
+  </div>
+
+  <div v-for="(productsList, i) in products" :key="i">
+    <h4>{{products[i]}}</h4>
+    <p>50 만원</p>
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      menu : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
+
 
 <style>
 #app {
@@ -21,6 +36,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu { 
+  background:  darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
 }
 </style>
