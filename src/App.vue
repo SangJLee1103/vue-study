50f630d9ca2f65aa3c49ba7f00366f9c7e6e401f
sangjun <jesusfollow1103@naver.com>
2021-12-20 22:03:46 +0900

    day1 v-if, modal

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
 
+<!-- modal -->
+<div class="black-bg" v-if="modalOpen == true">
+  <div class="white-bg"> 
+    <h4>상세페이지임</h4>
+    <p>상세페이지 내용임</p>
+    <button @click="modalOpen = false">닫기</button>
+  </div>
+</div>
+
 <!-- nav -->
   <div class="menu">
     <!-- :key=""의 용도 
@@ -7,29 +16,86 @@
     <a v-for="(menuList, i) in menu" :key="i"> {{ menuList }} </a>  
   </div>
 
-  <div v-for="(productsList, i) in products" :key="i">
+
+
+
+
+  <div>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="modalOpen = true">{{products[0]}}</h4>
+    <p>50 만원</p>
+    <!-- v-on과 @는 같음 -->
+    <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{count[0]}}</span>
+  </div>
+  <div>
+    <img src="./assets/room1.jpg" class="room-img">
+    <h4>{{products[1]}}</h4>
+    <p>60 만원</p>
+    <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{count[1]}}</span>
+  </div> 
+  <div>
+    <img src="./assets/room2.jpg" class="room-img">
+    <h4>{{products[2]}}</h4>
+    <p>70 만원</p>
+    <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{count[2]}}</span>
+  </div> 
+  <!-- <div v-for="(productsList, i) in products" :key="i">
     <h4>{{products[i]}}</h4>
     <p>50 만원</p>
-  </div> 
+  </div>  -->
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  data(){
+  data(){ //react에서는 state라고 함
     return {
+      modalOpen : false,
+      count: [0, 0, 0],
       menu : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
+  methods : {
+    increase(i){
+      this.count[i]++;
+    }
+  },
+
   components: {
   }
 }
 </script>
 
-
 <style>
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
+.room-img{
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
